5594f9e75519ccd7c1d51513daea0ccb294e454f
Rodrigo Ribeiro <a61481@alunos.ipb.pt>
2025-01-28 22:13:36 +0000

    add sidebar

---
+++ lib/components/side_bar.dart
@@ -0,0 +1,108 @@
+import 'package:farm_manager/login/login.dart';
+import 'package:farm_manager/service/authservice.dart';
+import 'package:farm_manager/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({
+    super.key,
+    required SidebarXController controller,
+  }) : _controller = controller;
+
+  final SidebarXController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SidebarX(
+      controller: _controller,
+      theme: SidebarXTheme(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: TAppTheme.appTheme.scaffoldBackgroundColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        hoverColor: TAppTheme.appTheme.canvasColor,
+        textStyle: TextStyle(
+            color: TAppTheme.appTheme.primaryColorLight.withOpacity(0.7)),
+        selectedTextStyle:
+            TextStyle(color: TAppTheme.appTheme.primaryColorLight),
+        hoverTextStyle: TextStyle(
+          color: TAppTheme.appTheme.canvasColor,
+          fontWeight: FontWeight.w500,
+        ),
+        itemTextPadding: const EdgeInsets.only(left: 30),
+        selectedItemTextPadding: const EdgeInsets.only(left: 30),
+        itemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: TAppTheme.appTheme.primaryColorLight),
+        ),
+        selectedItemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: TAppTheme.appTheme.primaryColorLight),
+          boxShadow: [
+            BoxShadow(
+              color: TAppTheme.appTheme.canvasColor.withOpacity(0.24),
+              blurRadius: 30,
+            )
+          ],
+        ),
+        iconTheme: IconThemeData(
+          color: TAppTheme.appTheme.primaryColorLight,
+          size: 20,
+        ),
+        selectedIconTheme: IconThemeData(
+          color: TAppTheme.appTheme.primaryColorLight,
+          size: 20,
+        ),
+      ),
+      extendedTheme: SidebarXTheme(
+        width: 200,
+        decoration: BoxDecoration(
+          color: TAppTheme.appTheme.scaffoldBackgroundColor,
+        ),
+      ),
+      footerItems: [
+        SidebarXItem(
+          icon: Icons.exit_to_app,
+          label: 'Loggout',
+          onTap: () async {
+            AuthService authService = AuthService();
+            await authService.signOut(context);
+            if (context.mounted) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const LoginPage(),
+                ),
+              );
+            }
+          },
+        ),
+      ],
+      items: [
+        SidebarXItem(
+          icon: Icons.home,
+          label: 'Home',
+          onTap: () {
+            debugPrint('Home');
+          },
+        ),
+        SidebarXItem(
+          icon: Icons.person_2,
+          label: 'Perfil',
+          onTap: () {
+            debugPrint('Perfil');
+          },
+        ),
+        SidebarXItem(
+          icon: Icons.minor_crash,
+          label: 'Máquinas',
+          onTap: () {
+            debugPrint('Máquinas');
+          },
+        ),
+      ],
+    );
+  }
+}
