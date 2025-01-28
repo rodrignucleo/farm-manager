44b50406b9c705b8f8b00dacb4befd4f60ccbee3
Rodrigo Ribeiro <a61481@alunos.ipb.pt>
2025-01-28 22:12:21 +0000

    add theme app

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData appTheme = ThemeData(
+    useMaterial3: true,
+    primaryColorLight: const Color(0xFFFFFFFF),
+    primaryColor: const Color(0xFF8C9EFF),
+    canvasColor: const Color(0xFF1A237E),
+    scaffoldBackgroundColor: const Color(0xFF14181B),
+    // scaffoldBackgroundColor: const Color(0xFFFFFFFF)
+  );
+}
