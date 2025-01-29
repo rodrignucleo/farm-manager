e88aae77bb3862ec8683a1e77065ecb99b6d5cc8
Rodrigo Ribeiro <a61481@alunos.ipb.pt>
2025-01-29 19:20:42 +0000

    fix theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -5,10 +5,24 @@ class TAppTheme {
 
   static ThemeData appTheme = ThemeData(
     useMaterial3: true,
+    disabledColor: const Color.fromARGB(255, 211, 68, 68),
     primaryColorLight: const Color(0xFFFFFFFF),
-    primaryColor: const Color(0xFF8C9EFF),
-    canvasColor: const Color(0xFF1A237E),
-    scaffoldBackgroundColor: const Color(0xFF14181B),
-    primaryColorDark: const Color.fromRGBO(20, 24, 27, 1),
+    primaryColor: const Color.fromARGB(255, 39, 144, 176),
+    canvasColor: const Color.fromARGB(255, 78, 77, 74),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 43, 78, 114),
+    navigationBarTheme: NavigationBarThemeData(
+      labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>(
+        (Set<WidgetState> states) {
+          if (states.contains(WidgetState.selected)) {
+            return TextStyle(
+                color: TAppTheme.appTheme.primaryColorLight,
+                fontWeight: FontWeight.bold);
+          }
+          return TextStyle(color: TAppTheme.appTheme.primaryColorLight);
+        },
+      ),
+    ),
+
+    // primaryColorDark: const Color.fromRGBO(20, 24, 27, 1),
   );
 }
