f7f5cafb1225f4bbcfe5d51ea630510638296fe8
Rodrigo Ribeiro <a61481@alunos.ipb.pt>
2025-01-29 00:35:23 +0000

    edit authservice

--- lib/service/authservice.dart
+++ lib/service/authservice.dart
@@ -74,6 +74,12 @@ class AuthService {
               querySnapshot.docs.first.data() as Map<String, dynamic>;
           if (data['id'] == null) {
             await doc.reference.update({'id': user.uid});
+            QuerySnapshot querySnapshotUpdate = await firestore
+                .collection('costumer')
+                .where('email', isEqualTo: user.email)
+                .get();
+            data =
+                querySnapshotUpdate.docs.first.data() as Map<String, dynamic>;
           }
           Costumer costumer = Costumer.fromMap(data);
           Provider.of<CostumerProvider>(context, listen: false)
