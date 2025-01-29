d862145fddd2ec57d6e8483d51cad9cca55dda07
Rodrigo Ribeiro <a61481@alunos.ipb.pt>
2025-01-29 00:34:50 +0000

    add machine model

--- lib/model/machine.dart
+++ lib/model/machine.dart
@@ -1,25 +1,22 @@
-class Machine {
+class MachineArray {
   final String frota;
   final String modelo;
   final String placa;
-  final String responsavel;
-  final String costumer;
+  final String? responsavel;
 
-  Machine({
+  MachineArray({
     required this.frota,
     required this.modelo,
     required this.placa,
-    required this.responsavel,
-    required this.costumer,
+    this.responsavel,
   });
 
-  factory Machine.fromMap(Map<String, dynamic> map) {
-    return Machine(
+  factory MachineArray.fromMap(Map<String, dynamic> map) {
+    return MachineArray(
       frota: map['frota'] as String,
       modelo: map['modelo'] as String,
       placa: map['placa'] as String,
       responsavel: map['responsavel'] as String,
-      costumer: map['costumer'] as String,
     );
   }
 
@@ -29,11 +26,40 @@ class Machine {
       'modelo': modelo,
       'placa': placa,
       'responsavel': responsavel,
-      'costumer': costumer,
     };
   }
 
   @override
   String toString() =>
-      'Costumer(frota: $frota, modelo: $modelo, placa: $placa, responsavel: $responsavel, costumer: $costumer)';
+      'Costumer(frota: $frota, modelo: $modelo, placa: $placa, responsavel: $responsavel)';
+}
+
+class Machine {
+  final String costumer;
+  final List<MachineArray>? machines;
+
+  Machine({
+    required this.costumer,
+    this.machines,
+  });
+
+  factory Machine.fromMap(Map<String, dynamic> map) {
+    return Machine(
+        costumer: map['costumer'] as String,
+        machines: (map['machine'] as List<dynamic>?)
+                ?.map((machine) =>
+                    MachineArray.fromMap(machine as Map<String, dynamic>))
+                .toList() ??
+            []);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'costumer': costumer,
+      'machines': machines?.map((machine) => machine.toMap()).toList(),
+    };
+  }
+
+  @override
+  String toString() => 'Costumer(costumer: $costumer, machines: $machines)';
 }
